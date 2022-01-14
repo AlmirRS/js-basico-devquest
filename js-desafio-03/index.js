89ab6440ca8f2ce9894da67df2001a25c26724dc
Almir Ramos <almirrdsnegro@gmail.com>
2022-01-14 00:00:29 -0300

    correção feita if

--- js-desafio-03/index.js
+++ js-desafio-03/index.js
@@ -1,5 +1,6 @@
+const vaiSeHospedar = confirm( "Seja bem vindo (a) aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?")
 
-if( confirm( "Seja bem vindo (a) aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?")){
+if(vaiSeHospedar){
     alert("Ótimo! Nós temos as melhores camas de toda a região!")
 }else{
     alert("Que pena! Você parecia ser uma pessoa mais legal")
